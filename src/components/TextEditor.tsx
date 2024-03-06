0569dac4fd520834691735326a34e3340277d7f5
bernardoenock <bernardoenockgm@gmail.com>
2024-03-06 15:47:06 -0300

    fix: Template

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Editor, EditorContent } from '@tiptap/react'
+import { EditorContent } from '@tiptap/react'
 import { Box } from '@mui/material'
+import { useEditorInstance } from '../hooks/useEditorInstance'
+import SelectedMenu from './Menus/SelectedMenu'
 
-type TextEditorProps = {
-  editor: Editor | null
-}
+// type TextEditorProps = {
+//   editor: Editor | null
+// }
 
-export default function TextEditor({editor}: TextEditorProps) {
-
+export default function TextEditor() {
+  const { editor } = useEditorInstance()
+  
   if (!editor) {
     return <>Algo deu errado... -_-</>
   }
@@ -18,11 +21,16 @@ export default function TextEditor({editor}: TextEditorProps) {
       m: 2,
       ".tiptap": {
         p: 2,
-        borderStyle: "solid",
-        borderRadius: '5px'
-      }
+        border: 'none',
+        boxShadow: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+        ":focus": {
+          outline: 'none',
+          boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px'
+        }
+      },
     }}>
         <EditorContent editor={editor} />
+        <SelectedMenu editor={editor}/>
     </Box>
   )
 }
