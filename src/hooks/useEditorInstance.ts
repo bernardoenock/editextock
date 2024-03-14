53e75e9be679451e9d7282d1d19c647401fc2bf9
bernardoenock <bernardoenockgm@gmail.com>
2024-03-14 10:39:41 -0300

    feat: Save content

--- src/hooks/useEditorInstance.ts
+++ src/hooks/useEditorInstance.ts
@@ -1,16 +1,27 @@
-import { Editor } from "@tiptap/react"
+import { Editor } from "@tiptap/react";
 import { create } from "zustand";
 import { editorT } from "./editorConfig";
-
 
 type EditorInstanceType = {
   editor: Editor | null;
   setEditor: (editor: Editor) => void;
-}
+  setContentHTML: (contentHTML: string) => void;
+  contentHTML: string;
+  setContentJson: (contentJson: object) => void;
+  contentJson: object;
+};
 
 export const useEditorInstance = create<EditorInstanceType>((set) => ({
   editor: editorT,
+  contentHTML: editorT.getHTML(),
+  contentJson: editorT.getJSON(),
   setEditor: (editor: Editor) => {
-    set({ editor })
-  }
-}))
+    set({ editor });
+  },
+  setContentHTML: (contentHTML: string) => {
+    set({ contentHTML });
+  },
+  setContentJson: (contentJson: object) => {
+    set({ contentJson });
+  },
+}));
