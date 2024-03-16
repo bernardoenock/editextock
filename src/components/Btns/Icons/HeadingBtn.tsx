009ba515d5718ee2287c8a25103cf30847e4d18a
bernardoenock <bernardoenockgm@gmail.com>
2024-03-16 15:54:22 -0300

    feat: Create Heading Btn

---
+++ src/components/Btns/Icons/HeadingBtn.tsx
@@ -0,0 +1,31 @@
+import TitleIcon from '@mui/icons-material/Title';
+import { useState } from "react";
+import IconBtnWrapper from '../../Wrappers/IconBtnWrapper';
+import HeadingModal from '../../Modals/HeadingModal';
+
+
+export default function HeadingBtn() {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  
+  const handleCloseHeadingModal = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOpenHeadingModal = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  return (
+    <>
+      <IconBtnWrapper
+        title="Heading"
+        onClick={handleOpenHeadingModal}>
+        <TitleIcon />
+      </IconBtnWrapper>
+      <HeadingModal 
+        anchorEl={anchorEl}
+        handleClose={handleCloseHeadingModal}
+      />
+    </>
+  );
+}
