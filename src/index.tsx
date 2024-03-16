9167a1ec519e668632867e51d26ec06d2c80381e
bernardoenock <bernardoenockgm@gmail.com>
2024-03-16 15:49:36 -0300

    feat: Update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,16 @@
+import { useEditorInstance } from "./hooks/useEditorInstance";
+
+import HeaderMenu from "./components/Menus/HeaderMenu";
+import LineMenu from "./components/Menus/LineMenu";
+import SelectedMenu from "./components/Menus/SelectedMenu";
+import TextEditor from "./components/TextEditor";
 import Template from "./components/Template";
 
-export { Template }
+export { 
+  useEditorInstance, 
+  HeaderMenu,
+  LineMenu,
+  SelectedMenu,
+  TextEditor,
+  Template, 
+}
