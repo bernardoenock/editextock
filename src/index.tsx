7c4347a68c7f5edc3f90dd71cc952ee31f1eba6b
bernardoenock <bernardoenockgm@gmail.com>
2024-03-19 16:59:47 -0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
-import { Content as ContentTipTap, Editor as EditorTipTap } from "@tiptap/react";
-import { editorConfig } from "./hooks/editorConfig";
-
-import {useEditorInstance, EditorProvider} from "./hooks/useEditorInstance";
+import { useEditorInstance, EditorProvider } from "./hooks/useEditorInstance";
 
 import HeaderMenu from "./components/Menus/HeaderMenu";
 import LineMenu from "./components/Menus/LineMenu";
@@ -9,15 +6,9 @@ import SelectedMenu from "./components/Menus/SelectedMenu";
 import TextEditor from "./components/TextEditor";
 import Template from "./components/Template";
 
-export type {
-  ContentTipTap,
-  EditorTipTap,
-}
-
 export { 
-  editorConfig,
-  EditorProvider,
   useEditorInstance, 
+  EditorProvider,
   HeaderMenu,
   LineMenu,
   SelectedMenu,
